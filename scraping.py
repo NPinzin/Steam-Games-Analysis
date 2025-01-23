1c45b10239a3a80b4032a2ea19510d15a22f507e
NPinzin <nick.pinzin@gmail.com>
2025-01-23 16:03:22 -0500

    appdetails by ID API call - 429 error code

--- scraping.py
+++ scraping.py
@@ -19,9 +19,29 @@ def get_apps():
         print("Error: ", e)
         return None
 
+def get_app_details(id):
+    """Uses https://store.steampowered.com/api/appdetails/?appids= to get details of a specific app
+    """
+    url = "http://store.steampowered.com/api/appdetails/?appids=" + str(id)
+
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            app = response.json()
+            return app
+        else:
+            print("Error: ", response.status_code)
+            return None
+
+    except requests.exceptions.RequestException as e:
+        print("Error: ", e)
+        return None
+
+
 def main():
     apps = get_apps()
     to_delete = []
+    apps_details = []
     if apps:
         for app in apps["applist"]["apps"]:
             id = app["appid"]
@@ -32,9 +52,17 @@ def main():
 
         for app in to_delete:
             apps["applist"]["apps"].remove(app)
+
+        for app in apps["applist"]["apps"]:
+            id = app["appid"]
+            app_details = get_app_details(id)
+            if app_details:
+                apps_details.append(app_details)
             
         with open("apps.json", "w") as file:
             json.dump(apps, file)
+        with open("apps_details.json", "w") as file:
+            json.dump(apps_details, file)
 
 
 if __name__ == "__main__":
